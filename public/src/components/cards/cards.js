dbf4295afce7bd2612e3283a6f0f985d4d3f7393
Manuelard03 <127862835+Manuelard03@users.noreply.github.com>
2023-09-08 16:21:30 -0500

    Add files via upload

--- public/src/components/cards/cards.js
+++ public/src/components/cards/cards.js
@@ -16,21 +16,21 @@ class cards extends HTMLElement {
         <header><h2>¿Por qué cambiarse a Premium?</h2></header>
         <ul>
         <li>
-            <div><img src="https://github.com/Manuelard03/Laboratorio-2-Algoritmos/blob/main/img/icono%201-spotify.PNG?raw=true"></div>
+            <div><img src="https://github.com/Manuelard03/Laboratorio-2-Algoritmos/blob/main/public/img/icono%201-spotify.PNG?raw=true"></div>
             <div>
                 <h3>Descarga tu música</h3>
                 <p>Escúchala desde cualquier lugar.</p>
             </div>
         </li>
         <li>
-            <div><img src="https://github.com/Manuelard03/Laboratorio-2-Algoritmos/blob/main/img/icono%202%20-spotify.PNG?raw=true"></div>
+            <div><img src="https://github.com/Manuelard03/Laboratorio-2-Algoritmos/blob/main/public/img/icono%202%20-spotify.PNG?raw=true"></div>
             <div>
                 <h3>Escucha música sin anuncios.</h3>
                 <p>Disfruta de tu música sin interrupciones.</p>
             </div>
         </li>
         <li>
-            <div><img src="https://github.com/Manuelard03/Laboratorio-2-Algoritmos/blob/main/img/icono%203-spotify.PNG?raw=true"></div>
+            <div><img src="https://github.com/Manuelard03/Laboratorio-2-Algoritmos/blob/main/public/img/icono%203-spotify.PNG?raw=true"></div>
             <div>
                 <h3>Reproduce canciones en el</h3>
                 <h3>orden que quieras.</h3>
@@ -38,7 +38,7 @@ class cards extends HTMLElement {
             </div>
         </li>
         <li>
-            <div><img src="https://github.com/Manuelard03/Laboratorio-2-Algoritmos/blob/main/img/icono%204-spotify.PNG?raw=true"></div>
+            <div><img src="https://github.com/Manuelard03/Laboratorio-2-Algoritmos/blob/main/public/img/icono%204-spotify.PNG?raw=true"></div>
             <div>
                 <h3>Mejor calidad de sonido.</h3>
                 <p>Siente el sonido.</p>
